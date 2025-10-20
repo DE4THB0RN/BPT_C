354137709ac1d704aa765d35d6ba3b44f541bd57
DE4THB0RN <pedroapferreirass@gmail.com>
2025-10-20 20:50:02 -0300

    Criação de arquivos principais

---
+++ principal.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <time.h>
+#include "pgmread.h"
+#include "bpt.h"
+#include "tarjan.h"
+#include "grafo.h"
+#include "lista.h"
+#include "utilidades.h"
+
+#include "imagem.h"
+
+int main()
+{
+    clock_t inicio, fim;
+    double tempo;
+
+    inicio = clock();
+    Grafo *grafo1 = grafo_arq("teste1.pgm");
+
+    // Grafo *grafo1 = novo_grafo(8);
+    // adicionar_aresta(0, 1, 95, grafo1);
+    // adicionar_aresta(0, 4, 0, grafo1);
+    // adicionar_aresta(0, 5, 100, grafo1);
+    // adicionar_aresta(1, 2, 120, grafo1);
+    // adicionar_aresta(1, 5, 80, grafo1);
+    // adicionar_aresta(1, 6, 180, grafo1);
+    // adicionar_aresta(2, 3, 9, grafo1);
+    // adicionar_aresta(2, 6, 125, grafo1);
+    // adicionar_aresta(2, 7, 200, grafo1);
+    // adicionar_aresta(3, 7, 116, grafo1);
+    // adicionar_aresta(4, 5, 15, grafo1);
+    // adicionar_aresta(4, 1, 50, grafo1);
+    // adicionar_aresta(5, 6, 75, grafo1);
+    // adicionar_aresta(5, 2, 46, grafo1);
+    // adicionar_aresta(6, 7, 0, grafo1);
+    // adicionar_aresta(6, 3, 96, grafo1);
+    printf("Grafo criado com sucesso!\n");
+    BPT *bpt = kruskal(grafo1);
+    fim = clock();
+
+    tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    printf("Tempo total de criação: %.2f s\n", tempo);
+
+    // Coisas para o processamento de teste1.pgm
+    int seed1 = processar_seed(grafo1, 113, 226);
+    int seed2 = processar_seed(grafo1, 1381, 294);
+    int seed3 = processar_seed(grafo1, 359, 183);
+    int seed4 = processar_seed(grafo1, 296, 512);
+    int seed5 = processar_seed(grafo1, 1890, 296);
+    int seed6 = processar_seed(grafo1, 1472, 959);
+    int seed7 = processar_seed(grafo1, 897, 309);
+
+    int seeds[] = 
+    {
+        seed1,
+        seed6,
+        seed3, 
+        seed4, 
+        seed5, 
+        seed2, 
+        seed7
+        };
+    int numseeds = 7;
+
+    printf("Processando seeds...\n");
+    adicionar_seeds(seeds, numseeds, bpt, grafo1);
+    printf("Teoricamente sem problemas\n");
+
+    salvar_imagem("resultado1_7s.png", grafo1);
+
+    return 0;
+}
---
+++ tarjan.h
@@ -0,0 +1,140 @@
+#include <stdlib.h>
+#include "bpt.h"
+#include "grafo.h"
+#ifndef KRSUKAL_H
+#define KRSUKAL_H
+
+int find(int *parent, int q)
+{
+    int r = q, tmp;
+
+    while (parent[r] >= 0)
+        r = parent[r];
+    while (parent[q] >= 0)
+    {
+        tmp = q;
+        q = parent[q];
+        parent[tmp] = r;
+    }
+
+    return r;
+}
+
+void swap(int *rank, int x, int y)
+{
+    int temp = rank[x];
+    rank[x] = rank[y];
+    rank[y] = temp;
+}
+
+void union_k(int *parent, int *rank, int x, int y)
+{
+    if (rank[x] > rank[y])
+        swap(rank, x, y);
+    if (rank[x] == rank[y])
+        rank[y]++;
+    parent[x] = y;
+}
+
+int compara_peso(const void *a, const void *b)
+{
+    MST_Edge *no1 = (MST_Edge *)a;
+    MST_Edge *no2 = (MST_Edge *)b;
+
+    if (no1->peso < no2->peso)
+        return -1;
+    else if (no1->peso > no2->peso)
+        return 1;
+    else
+        return 0;
+}
+
+BPT *kruskal(Grafo *gr)
+{
+    int num_vertices = gr->numVertices;
+    BPT *bpt = criar_bpt(num_vertices);
+
+    MST_Edge_List *lista_arestas = arestas_MST(gr);
+
+    qsort(lista_arestas->arestas, lista_arestas->numArestas, sizeof(MST_Edge), compara_peso);
+
+    if (lista_arestas->arestas == NULL)
+    {
+        free(bpt);
+        printf("Erro ao alocar memória para arestas.\n");
+        return NULL;
+    }
+
+    int *parent = (int *)malloc(sizeof(int) * num_vertices);
+    int *rank = (int *)malloc(sizeof(int) * num_vertices);
+
+    NoBPT **raizes = (NoBPT **)malloc(sizeof(NoBPT *) * num_vertices);
+
+    for (int i = 0; i < num_vertices; i++)
+    {
+        parent[i] = -1;
+        rank[i] = 0;
+    }
+
+    int edge_count = 0;
+    int index = 0;
+    int x, y;
+    NoBPT *pai1, *pai2;
+    MST_Edge proxima;
+
+    while (edge_count < num_vertices - 1)
+    {
+        proxima = lista_arestas->arestas[index++];
+
+        x = find(parent, proxima.de);
+        y = find(parent, proxima.para);
+
+        if (x != y)
+        {
+
+            if (bpt->folhas[proxima.de] == NULL)
+            {
+                bpt->folhas[proxima.de] = criar_no_bpt_v(proxima.de);
+                raizes[proxima.de] = bpt->folhas[proxima.de];
+            }
+
+            if (bpt->folhas[proxima.para] == NULL)
+            {
+                bpt->folhas[proxima.para] = criar_no_bpt_v(proxima.para);
+                raizes[proxima.para] = bpt->folhas[proxima.para];
+            }
+
+            NoBPT *novo_no = criar_no_bpt_e(proxima.de, proxima.para, proxima.peso);
+
+            pai1 = raizes[x];
+            pai2 = raizes[y];
+
+            raizes[x] = novo_no;
+            raizes[y] = novo_no;
+
+            novo_no->esq = pai1;
+            novo_no->dir = pai2;
+            pai1->pai = novo_no;
+            pai2->pai = novo_no;
+
+            marcar_mst(proxima.de, proxima.para, gr);
+
+            union_k(parent, rank, x, y);
+
+            edge_count++;
+        }
+    }
+
+    int root = find(parent, 0);
+    bpt->raiz = raizes[root];
+
+    free(lista_arestas->arestas);
+    free(lista_arestas);
+    free(parent);
+    free(rank);
+    free(raizes);
+
+    return bpt;
+}
+
+#endif // KRSUKAL_H
